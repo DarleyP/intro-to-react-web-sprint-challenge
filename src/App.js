0cd925c9697208f46c0ecf69e171e72557fba2ea
Darley Peralta <ordeal00s@gmail.com>
2023-05-07 20:14:32 -0400

    DarleyP

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { BASE_URL } from './constants';
+import Character from './components/Character';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+ const [characters, setCharacters] = useState([])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+ 
+
+useEffect(() => {
+  axios.get(`${BASE_URL}`)
+.then(res=>
+  setCharacters(res.data))
+.catch(err=>
+  console.err(err))
+}, [])
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+     {characters.map( character => {
+      return <Character info={character} key={character.name} />
+     })
+     }
     </div>
   );
 }
 
 export default App;
+
+
+ // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,45 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledCharacter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 2px solid black;
+  border-radius: 10px;
+  padding: 10px;
+  font-family: 'Helvetica', sans-serif;
+  font-size: 1.2rem;
+  font-weight: bold;
+  background-color: rgba(128, 128, 128, 0.3);
+  margin: 10px auto;
+  width: 50%;
+`;
+
+const StyledH3 = styled.h3`
+  margin: 0;
+  font-family: 'Verdana', sans-serif;
+  font-size: 1.5rem;
+`;
+
+const StyledH4 = styled.h4`
+  margin: 0;
+  font-family: 'Verdana', sans-serif;
+  font-size: 1.2rem;
+`;
+const Character = props => {
+   
+
+    return (
+        <StyledCharacter>
+            <h3>{props.info.name}</h3>   <h4>{props.info.birth_year}</h4>
+        </StyledCharacter>
+    )
+}
+
+
+export default Character;
+
+
+//  
